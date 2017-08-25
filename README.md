a8edc21d0d820aefba05b655aa4a283bdcdb56b3
molenzwiebel <info@thijsmolendijk.nl>
2017-08-25 17:00:16 +0200

    Initial file structure

---
+++ README.md
@@ -0,0 +1,3 @@
+# Sweeping Lens
+
+Monitors all Riot traffic automatically and gives you a neat web dashboard.
---
+++ frontend/webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = function(env) {
+    return {
+        entry: env === "prod" ? ["./src/index.ts"] : [
+            "webpack-dev-server/client?http://0.0.0.0:8081",
+            "webpack/hot/only-dev-server",
+            "./src/index.ts"
+        ],
+        output: {
+            filename: "bundle.js",
+            path: path.resolve(__dirname, "dist")
+        },
+        module: {
+            rules: [{
+                test: /\.tsx?$/,
+                loader: "ts-loader",
+                options: { appendTsSuffixTo: [/\.vue$/] }
+            }, {
+                test: /\.vue$/,
+                loader: "vue-loader"
+            }, {
+                test: /\.json$/,
+                loader: "file-loader"
+            }, {
+                test: /\.png$/,
+                loader: "file-loader"
+            }, {
+                test: /\.jpg$/,
+                loader: "file-loader"
+            }, {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            }]
+        },
+        resolve: {
+            extensions: [".js", ".json", ".ts", ".html", ".vue"]
+        },
+        devServer: {
+            hot: true,
+            contentBase: path.resolve(__dirname, "src"),
+            publicPath: "/",
+            port: 8081
+        },
+        plugins: [
+            new webpack.HotModuleReplacementPlugin()
+        ]
+    };
+};
